4949ec890a7b6a4f6d59dfb40d7cd7494a4c2382
Gianluca Moriconi <66262520+gianlucamoriconi@users.noreply.github.com>
2022-08-02 02:46:46 -0300

    Update main-js-v2.js

--- main-js-v2.js
+++ main-js-v2.js
@@ -53,7 +53,7 @@ function selection(name, price, category){
   newItem.setAttribute('id', name);
   newItem.setAttribute('data-price', price);
   newItem.innerHTML = '<p class="col-8 fs-7 fw-bold mb-0">'+name+'</p><p class="item-price d-flex justify-content-end col-4 fs-7 fw-light mb-0">$'+price+'<a class="delete-icon"><svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" class="delete-icon-svg"><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"></path></svg></a></p>';
-  newItem.classList.add('item', 'd-flex', 'shadow', 'bg-white', 'rounded-3', 'mb-4', 'me-2', 'p-3', 'fs-7');
+  newItem.classList.add('item', 'd-flex', 'shadow', 'bg-white', 'rounded-3', 'mb-4', 'ms-2', 'me-2', 'p-3', 'fs-7');
   document.querySelector(".content-order").append(newItem);
   sumOfItems();
 }
